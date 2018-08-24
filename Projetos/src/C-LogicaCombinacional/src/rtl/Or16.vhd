e3f78380a6354197642c35d80cd670ccf045e376
Fogelman <ddfogelman@gmail.com>
2018-08-24 08:47:43 -0300

    Entrega Or16 OK

--- Projetos/src/C-LogicaCombinacional/src/rtl/Or16.vhd
+++ Projetos/src/C-LogicaCombinacional/src/rtl/Or16.vhd
@@ -7,3 +7,9 @@ entity Or16 is
 			b:   in  STD_LOGIC_VECTOR(15 downto 0);
 			q:   out STD_LOGIC_VECTOR(15 downto 0));
 end entity;
+
+architecture rtl of Or16 is
+
+begin
+   q <=  a or b;
+end rtl;
