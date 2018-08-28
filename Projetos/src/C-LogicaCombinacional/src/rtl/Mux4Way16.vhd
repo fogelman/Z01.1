c20deceb3ddb7a852ebe6744d23d8245d49510f5
ThunderSly <brunex887@gmail.com>
2018-08-28 16:13:33 -0300

    nor8way e mux4way16 adicionado e testado dog

--- Projetos/src/C-LogicaCombinacional/src/rtl/Mux4Way16.vhd
+++ Projetos/src/C-LogicaCombinacional/src/rtl/Mux4Way16.vhd
@@ -9,4 +9,20 @@ entity Mux4Way16 is
 			d:   in  STD_LOGIC_VECTOR(15 downto 0);
 			sel: in  STD_LOGIC_VECTOR(1 downto 0);
 			q:   out STD_LOGIC_VECTOR(15 downto 0));
-end entity;
+end Mux4Way16;
+
+architecture behavior of Mux4Way16 is
+	begin
+	   	process(a,b,c,d,sel)
+	   	begin
+		   	if (sel = "00") then
+			   q <= a;
+		   	elsif (sel = "01") then
+			   q <= b;
+		   	elsif (sel = "10") then
+			   q <= c;
+		   	elsif (sel = "11") then
+			   q <= d;
+			end if;
+		end process;
+end behavior;
--- Projetos/src/C-LogicaCombinacional/src/rtl/Nor8Way.vhd
+++ Projetos/src/C-LogicaCombinacional/src/rtl/Nor8Way.vhd
@@ -12,4 +12,10 @@ entity Nor8Way is
 			g:   in  STD_LOGIC;
 			h:   in  STD_LOGIC;
 			q:   out STD_LOGIC);
-end entity;
+end Nor8Way;
+
+architecture behavior of Nor8Way is
+begin
+	q <=not( a or b or c or d or e or f or g or h);
+
+end behavior;
