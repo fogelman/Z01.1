6d9b0cbb44096c928ab47160cd8d5af7fcf9fddd
heliopn <hpaivan@gmail.com>
2018-10-24 13:43:10 -0300

    SymbolT testado

--- Projetos/src/H-Assembler/Assembler/src/main/java/assembler/SymbolTable.java
+++ Projetos/src/H-Assembler/Assembler/src/main/java/assembler/SymbolTable.java
@@ -5,16 +5,46 @@
 
 package assembler;
 
+import java.util.HashMap;
+import java.util.Map;
 /**
  * Mantém uma tabela com a correspondência entre os rótulos simbólicos e endereços numéricos de memória.
  */
 public class SymbolTable {
 
     /**
-     * Cria a tabela de símbolos.
+     * Cria a tabela de Simbolos utilizada na RAM
      */
+	
+	Map<String,Integer> Table = new HashMap<String,Integer>();
+	
     public SymbolTable() {
-
+    	Table.put("R0", 0);
+    	Table.put("R1", 1);
+    	Table.put("R2", 2);
+    	Table.put("R3", 3);
+    	Table.put("R4", 4);
+    	Table.put("R5", 5);
+    	Table.put("R6", 6);
+    	Table.put("R7", 7);
+    	Table.put("R8", 8);
+    	Table.put("R9", 9);
+    	Table.put("R10", 10);
+    	Table.put("R11", 11);
+    	Table.put("R12", 12);
+    	Table.put("R13", 13);
+    	Table.put("R14", 14);
+    	Table.put("R15", 15);
+//    	Simbolos Especiais
+    	Table.put("SP", 0);
+    	Table.put("LCL", 1);
+    	Table.put("ARG", 2);
+    	Table.put("THIS", 3);
+    	Table.put("THAT", 4);
+    	Table.put("SCREEN", 16384);
+    	Table.put("LED", 21184);
+    	Table.put("SW", 21185);
+    	
     }
 
     /**
@@ -23,7 +53,10 @@ public class SymbolTable {
      * @param  address símbolo a ser armazenado na tabela de símbolos.
      */
     public void addEntry(String symbol, int address) {
-
+      /**
+       * Adiciona novas variaveis
+       **/
+    	Table.put(symbol, address);
     }
 
     /**
@@ -32,7 +65,11 @@ public class SymbolTable {
      * @return Verdadeiro se símbolo está na tabela de símbolos, Falso se não está na tabela de símbolos.
      */
     public Boolean contains(String symbol) {
-    	return null;
+        /**
+         * verifica se ja tem uma var
+         **/
+    	
+    	return Table.containsKey(symbol);
     }
 
     /**
@@ -41,7 +78,10 @@ public class SymbolTable {
      * @return valor numérico associado ao símbolo procurado.
      */
     public Integer getAddress(String symbol) {
-    	return null;
+        /**
+         * Puxando variaveis
+         **/
+    	return Table.get(symbol);
     }
 
 }
